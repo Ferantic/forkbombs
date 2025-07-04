16c30541762ca2036be4cf895b9cfb4b79cf1d76
Ferantic <silvermo2010@gmail.com>
2025-07-04 12:12:26 +0100

    Create destructive.py

---
+++ destructive.py
@@ -0,0 +1,9 @@
+import os
+i = 0
+while i == 0:
+  os.system("explorer.exe")
+  os.system("calculator.exe")
+  os.system("notepad.exe")
+  os.system("utilman.exe")
+  os.system("chrome.exe")
+      i += 1
